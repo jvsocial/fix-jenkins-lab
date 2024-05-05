ec547a13b0ace1e29599e79429c2315019be9233
jvsocial <78414554+jvsocial@users.noreply.github.com>
2024-05-05 19:35:35 +0530

    Update fix.sh

--- fix.sh
+++ fix.sh
@@ -1,4 +1,5 @@
-sudo wget https://get.jenkins.io/war/2.456/jenkins.war
+#sudo wget https://get.jenkins.io/war/2.456/jenkins.war
+sudo wget https://get.jenkins.io/war-stable/2.440.3/jenkins.war
 echo "Downloaded Jenkins"
 sudo cp jenkins.war /usr/share/jenkins
 echo "Preparing Jenkins"
